4986118f2d51a3a0ef7b63a54525d6ad3ae74243
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-05 18:54:00 -0500

    added post and get route

--- routing/itemRoutes.js
+++ routing/itemRoutes.js
@@ -14,10 +14,41 @@ const { items } = require("../fakeDb");
  *
 */
 
-router.get("", function(req, res, next) {
+router.get("", function(req, res) {
   res.json({items});
 });
 
+router.post("", function(req, res){
+  // console.log(req.body);
+  const newItem = req.body
+
+  items.push(newItem)
+  // console.log(items);
+  res.status(201).json({added: newItem})
+
+});
+
+
+router.get("/:name", function(req, res){
+
+  const name = req.params.name;
+  console.log("name", name);
+
+  for(const item of items){
+    console.log("item", item);
+    if (name === item.name){
+      res.json(item)
+    }
+  }
+
+  throw new NotFoundError("Item not found!");
+});
+
+//for each obj in array
+//  if obj.name === param name
+//    return obj
+//
+//return error
 
 
 
